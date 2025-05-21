9554dd391d11df743971de43de8297e715326e8b
AfrodCZ <slama@flexisoftware.cz>
2025-05-21 08:45:56 +0200

    Edited API test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from api.todo_api import app
+from app.fapi import app
 from app.storage import save_all_tasks
 from app.logic import Task
 
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 def test_update_task_status():
 
-    task = Task("Test Úkol", "Popis", "2025-06-01", "Střední")
+    task = Task("Test Úkol", "Popis", "01.06.2025", "Medium")
     save_all_tasks([task])  # uloží jeden task do CSV
 
     response = client.put("/update", json={"title": "Test Úkol"})
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,7 +3,7 @@ from app.logic import Task
 def test_create_task():
     task = Task("Test", "Popis", "2025-01-01", "Vysoká")
     assert task.title == "Test"
-    assert task.description == "Popis"
-    assert task.date == "2025-01-01"
-    assert task.priority == "Vysoká"
+    assert task.description == "Desc"
+    assert task.date == "01.12.2025"
+    assert task.priority == "High"
     assert task.status == "New"
